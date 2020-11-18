157ced7c335973b9db1e9993ea144246481f64ec
Caique Santos <caisbrito2003@gmail.com>
2020-11-18 20:46:11 -0300

    Exercício 1 feito

---
+++ Programa 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Programa_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            do{
+            Console.WriteLine("Digite um número entre 0 e 10:");
+                nota = int.Parse(Console.ReadLine());
+
+                if(nota <0 || nota >10){
+                    Console.WriteLine("Algarismo inválido!");
+                }
+        }   
+            while(nota <0 || nota >10);
+            Console.WriteLine("Algorismo valido!");
+        }
+    }
+}
