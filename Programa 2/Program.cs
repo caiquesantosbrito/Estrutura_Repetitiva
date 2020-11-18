fbbc00f8cc3311940fd611f4f46831cd574bcdd6
Caique Santos <caisbrito2003@gmail.com>
2020-11-18 20:59:43 -0300

    Exercício 2 feito

---
+++ Programa 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Programa_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            string senha = "";
+
+            while(true){
+                Console.WriteLine("Digite seu nome:");
+                nome= Console.ReadLine();
+
+                Console.WriteLine("Digite sua senha:");
+                senha = Console.ReadLine();
+
+                if(nome == senha){
+                    Console.WriteLine("Você não pode usar a mesma palavra para nome e senha!");
+                }else{break;}
+            }
+            Console.WriteLine("Bem vindo "+ nome +"!");
+        }
+    }
+}
