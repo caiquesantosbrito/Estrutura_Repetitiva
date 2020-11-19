eb135b507d3961696ae5de61089b1f29c65d3c90
Caique Santos <caisbrito2003@gmail.com>
2020-11-18 21:25:17 -0300

    Fibonacci feito

---
+++ Programa 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Programa_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero :");
+            int num;
+                int x=1, y=0, z=0;
+                    num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++){
+            z = x + y;
+            Console.Write( z + " ,");
+            y = x;
+            x = z;
+                }
+                Console.ReadLine();
+        }
+    }
+}
